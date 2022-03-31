01aaa8bfdb724e0a1f7fa2bfc8b08fc3ef30eaca
chenduoru <daychanhk@gmail.com>
2022-04-01 00:20:03 +0800

    update

--- CodeCraft-2022/src/main.cpp
+++ CodeCraft-2022/src/main.cpp
@@ -1,10 +1,12 @@
-#define DEBUG
+// #define DEBUG
 // 提交时记得注释掉这个宏
 #include "ContestIO.h"
 #include "Calculate.h"
 int main() {
     ContestCalculate cal;
-    cal.brute_force();
+    cal.brute_force2();
     cal.handle_output();
+    #ifdef DEBUG
     std::cout << "SCORE: " << cal.calculate_94_score() << std::endl;
+    #endif
 }
